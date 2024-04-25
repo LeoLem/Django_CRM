a4e1b06fe913104bb0299ede91c03bcad7ec7db8
Leo Lem <leo02310@gmail.com>
2024-04-25 12:06:42 +0200

    'registration'

--- mydb.py
+++ mydb.py
@@ -1,11 +1,11 @@
-import mysql.connector
+import mysql.conndector
 
 dataBase = mysql.connector.connect(
     host = 'localhost',
     user = 'root',
     passwd = 'Latitude',
     
-)
+) 
 
 #prepare the cursor object
 cursorObject = dataBase.cursor()
